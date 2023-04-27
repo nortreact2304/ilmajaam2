1b53f2757a2d0f51301d24fd5a4c68e6f54d9d61
Andres Järviste <andres.jarviste@wbscodingschool.com>
2023-04-27 19:28:39 +0300

    loeng07

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Asukohad from './Asukohad';
 import AsukohtDetailid from './AsukohtDetailid';
 import {loeAndmed} from './kontrollerid/andmed.js'
+import LisaAsukoht from './LisaAsukoht';
 
 function App() {
   const [aktiivne, setAktiivne] = useState(0)
@@ -27,6 +28,23 @@ function App() {
     },
   ])
 
+  const lisaKoht = (nimi, lat, long) => {
+    const uusKoht = {
+        nimi: nimi,
+        lat: lat,
+        long: long,
+        andmed: null
+      }
+    const uusMassiiv = [...asukohad, uusKoht]
+    setAsukohad(uusMassiiv)
+  }
+
+  useEffect(() => {
+    console.log('renderdus!')
+    muudaAktiivset(1);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   const asendaIlmaAndmed = (asukohaIndex, ilmaAndmed) => {
       const uuedAndmed = asukohad.map((asukoht, index) => {
         if (index !== asukohaIndex) {
@@ -37,7 +55,9 @@ function App() {
           temperatuur: ilmaAndmed.temperature,
           aeg: ilmaAndmed.time,
           ilmakood: ilmaAndmed.weathercode,
-          tuuleKiirus: ilmaAndmed.windspeed
+          tuuleKiirus: ilmaAndmed.windspeed,
+          kasPaev: ilmaAndmed.is_day,
+          tuuleSuund: ilmaAndmed.winddirection
         }
 
         return {...asukoht, andmed: ilm}
@@ -48,9 +68,10 @@ function App() {
 
   const muudaAktiivset = async (uusAktiivneIndeks) => {
     setAktiivne(uusAktiivneIndeks)
+    const valitudAsukoht = asukohad[uusAktiivneIndeks]
     const ilmaAndmed = await loeAndmed(
-      asukohad[uusAktiivneIndeks].lat,
-      asukohad[uusAktiivneIndeks].long,
+      valitudAsukoht.lat,
+      valitudAsukoht.long,
     )
     asendaIlmaAndmed(uusAktiivneIndeks, ilmaAndmed)
   }
@@ -69,9 +90,12 @@ function App() {
               asukohad={asukohad}
               muudaAktiivset={muudaAktiivset}
             />
+            <LisaAsukoht lisaKoht={lisaKoht}/>
           </div>
           <div className='col-12 col-sm-7'>
-            <AsukohtDetailid asukoht={asukohad[aktiivne]} />
+            <AsukohtDetailid 
+              asukoht={asukohad[aktiivne]} 
+            />
           </div>
 
         </div>
--- src/AsukohtDetailid.js
+++ src/AsukohtDetailid.js
@@ -2,6 +2,11 @@ function AsukohtDetailid({asukoht}) {
     if (asukoht.andmed == null) {
         return <h3>Laen andmeid ... </h3>
     }
+
+    let paev = "päev"
+    if (asukoht.andmed.kasPaev !== 1) {
+        paev = "öö";
+    }
     return (
         <>
             <h2>
@@ -11,6 +16,9 @@ function AsukohtDetailid({asukoht}) {
                 <p>Aeg: {asukoht.andmed.aeg}</p>
                 <p>Temperatuur: {asukoht.andmed.temperatuur}</p>
                 <p>Tuule kiirus: {asukoht.andmed.tuuleKiirus}</p>
+                <p>{paev}</p>
+                <p>{asukoht.andmed.tuuleSuund}</p>
+                
             </div>
 
         </>
